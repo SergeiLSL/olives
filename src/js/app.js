48b608f8d26a5da1fb45c5f7c722c7ff709ffe94
Sergei Student <lsl1959@mail.ru>
2023-05-16 19:47:09 +0600

    finish 90% project. Осталось подключить видео

--- src/js/app.js
+++ src/js/app.js
@@ -193,7 +193,7 @@ import * as flsScroll from "./files/scroll/scroll.js";
 
 // Функціонал додавання класів до хедеру під час прокручування
 // Документація: https://template.fls.guru/template-docs/modul-dobavleniya-klassov-k-shapke-pri-prokrutke-stranicy.html
-// flsScroll.headerScroll();
+flsScroll.headerScroll();
 
 // Модуль анімація цифрового лічильника
 // Документація: https://template.fls.guru/template-docs/modul-animacii-cifrovogo-lichilnika.html
